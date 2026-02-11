8ac66f375b34bd904fe066e7eba4b2510003da84
peterthorpe5 <pthorpe001@dundee.ac.uk>
2026-02-11 12:02:25 +0000

    RNAseq data filters

--- mk_final_results.sh
+++ mk_final_results.sh
@@ -8,4 +8,4 @@ BASE_DIR="${HOME}/data/2026_sperm_Gates"
 python ~/data/2026_sperm_Gates/PT_fertility_genomics/make_master_fertility_results_summary.py \
   --base_dir ~/data/2026_sperm_Gates \
   --testis_run_id testis_tau0.95_testisTPM5_presentTPM5_spermTPM0.1 \
-  --out_xlsx ~/data/2026_sperm_Gates/results/master_workbook/master_fertility_evidence.xlsx
+  --out_xlsx ~/data/2026_sperm_Gates/results/FULL_SUMMARY/SUMMARY_fertility_evidence.xlsx
