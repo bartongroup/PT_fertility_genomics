721b46e46cbd49b5d80bc5899e7b66aeb22dcde4
peterthorpe5 <pthorpe001@dundee.ac.uk>
2026-02-11 13:44:09 +0000

    plots

--- mk_final_results.sh
+++ mk_final_results.sh
@@ -14,7 +14,7 @@ python ~/data/2026_sperm_Gates/PT_fertility_genomics/make_master_fertility_resul
 
 # make plots
 
-python /home/pthorpe001/data/2026_sperm_Gates/scripts/make_fertility_gene_set_plots.py \
-  --in_xlsx /home/pthorpe001/data/2026_sperm_Gates/results/master_workbook/master_fertility_genes.xlsx \
+python ~/data/2026_sperm_Gates/PT_fertility_genomics//make_fertility_gene_set_plots.py \
+  --in_xlsx /home/pthorpe001/data/2026_sperm_Gates/results/FULL_SUMMARY/SUMMARY_fertility_evidence.xlsx \
   --sheet_name Tier_Summary_With_Omics \
-  --out_dir /home/pthorpe001/data/2026_sperm_Gates/results/master_workbook/plots
+  --out_dir /home/pthorpe001/data/2026_sperm_Gates/results/FULL_SUMMARY/plots
